78c3ef8259ba8c6572d1e2db08467ce6ed45ffc2
Andy MacKay <binarygeometry@gmail.com>
2024-04-18 22:09:55 +0100

    some style for the page view partial

--- src/components/full-page-image.tsx
+++ src/components/full-page-image.tsx
@@ -1,26 +1,21 @@
-import Image from "next/image";
+// import Image from "next/image";
 import { getImage } from "~/server/queries";
 import { Modal } from "~/components/modal";
 
-export default async function FullPageImageView(props: { id: number }){
-
-    // const idAsNumber = Number(id);
-    
-    // if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
+export default async function FullPageImageView(props: { id: string }){
 
     const image = await getImage(props.id);
 
     return (
         
-        <div>
-            <Image
-            className="w-96"
-            src={image.url} 
-            alt={image.name} 
-            width={480} 
-            height={480} 
-            style={{objectFit:"contain"}}
-            />
+        <div className="flex w-full h-full min-w-0">
+            <div className="flex flex-shrink justify-center items-center">
+                <img className="object-contain flex-shrink" src={image.url} alt={image.name} />
+            </div>
+
+            <div className="flex w-48 flex-shrink-0 flex-col border-l">
+                <div className="text-xl font-bold">{image.name}</div>
+            </div>
         
         </div>
     );
